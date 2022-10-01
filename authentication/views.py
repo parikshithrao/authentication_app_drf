6411ab120032d9c67ed45d42abd3d6ae782f48cf
parikshithrao <parikshithrao15@gmail.com>
2022-10-01 13:37:37 +0530

    Email Verification code added

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .serializers import RegisterSerializer
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
 # Create your views here.
 
 
@@ -16,4 +21,18 @@ class RegisterView(generics.GenericAPIView):
         serializer.save()
         user_data = serializer.data
 
+        user = User.objects.get(email = user_data['email'])
+        token = RefreshToken.for_user(user).access_token
+        current_site = get_current_site(request).domain
+        relativeLink = reverse('email-verify')
+        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+        email_body = "Hi" + user.username +  "Use Below link to verify your email\n"  + absurl 
+        data = {"email_body" : email_body, "to_email" : user.email, "email_subject" : "Verify your Email"}
+        Util.send_email(data)
+
         return Response(user_data, status = status.HTTP_201_CREATED)
+
+
+class VerifyEmail(generics.GenericAPIView):
+    def get(self):
+        pass
